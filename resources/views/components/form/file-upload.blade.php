046dae7e49167ea1d55d750a6d46b7f81ed34f29
ananditaputri <ananditaputri1307@gmail.com>
2025-12-22 21:00:42 +0700

    feat: reusable component

--- resources/views/components/form/file-upload.blade.php
+++ resources/views/components/form/file-upload.blade.php
@@ -57,7 +57,6 @@
 function previewImage(input, previewId) {
     const preview = document.getElementById(previewId);
     const file = input.files[0];
-    
     if (file) {
         const reader = new FileReader();
         reader.onload = function(e) {
